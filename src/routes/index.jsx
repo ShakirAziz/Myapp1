651d5884f3ed8a2c0e0af82ff3f870b0cc4feeed
Shakir Aziz <shakiraziz686@gmail.com>
2023-06-21 19:15:10 +0500

    first commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const Routers = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="" element />
+      </Routes>
+    </Router>
+  );
+};
+
+export default Routers;
