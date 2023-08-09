775e4e8e3bfaff41676699f07cbcd682ee38e0aa
Shakir Aziz <shakiraziz686@gmail.com>
2023-08-10 02:36:59 +0500

    first commit

--- src/page/LogIn/index.jsx
+++ src/page/LogIn/index.jsx
@@ -4,31 +4,38 @@ const LogIn = () => {
   return (
     <>
       <div className="form_outer_field">
+        <div className="middle_field">
+          <div>
+            <img
+              className="middle-image"
+              src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg"
+              alt=""
+            />
+          </div>
+          <div className="middle-paragraph">
+            <p className="para_1">Recents Logins</p>
+            <p>Click your picture or add an account</p>
+          </div>
+        </div>
         <div className="form_inner_field">
           <form action="#" className="logIn_form">
-            <div className="section-A">
-              <h2>Ship at shipped moves</h2>
-            </div>
             <div className="section-B">
-              <Input placeholder="john@gmail..." htmlFor="Gmail" />
+              <Input type="Gmail" placeholder="Email address or phone number" />
               <div>
                 <Input
-                  placeholder="********"
-                  htmlFor="Password"
+                  type="password"
+                  placeholder="Password"
                   maxlength="12"
                   required
                 />
-                <div className="input-checkbox">
-                  <div>
-                    <input type="checkbox" name="#" id="#" />
-                    <label htmlFor="#">Remember me</label>
-                  </div>
-                  <a href="#">Forgot password?</a>
-                </div>
               </div>
               <div className="submit-field">
                 <Input className="submit-button" type="submit" value="Log In" />
-                <a href="#">Create a new account</a>
+                <a href="#">Forget password?</a>
+              </div>
+              <div className="line-break"></div>
+              <div className="create-button">
+                <button>Create new account</button>
               </div>
             </div>
           </form>
